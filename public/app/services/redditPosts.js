02e98afd5abd994a292119dcfba6275bc8264eb0
Mika Aguilar <mika.aguilar@gmail.com>
2014-11-13 16:47:31 -0600

    Added private 'ids' service, checking imgur links for extensions
    The ids service is required, but .gitignored because it's private info.  redditPosts cycles through the post links and updates if needed, but only if no extension is found on imgur links.  Since it uses a promise, it isn't updating right.  Working on it.

--- public/app/services/redditPosts.js
+++ public/app/services/redditPosts.js
@@ -1,20 +1,34 @@
 'use strict';
 
-squareddit.factory('posts', ['$http', function postsFactory($http) {
+squareddit.factory('posts', ['$http', 'ids', function postsFactory($http, ids) {
     var current = {
             hot: [],
         },
         currentSub = 'test',
         processImages = function (data) {
-            //todo this should add img tags/change for flickr and imgur links with no extension
+            for (var i = 0; i < data.length; i++) {
+                var post = data[i].data,
+                    hasExt = (/\.(gif|jpg|jpeg|tiff|png)$/i).test(post.url);
+                console.log(post.domain);
+                //check for imgur links without extention, and for now just add .jpg
+                if (post.domain.search("imgur.com") != -1 && hasExt === false) {
+                    var imageId = post.url.match(/[^\/]*$/)[0];
+                    $http.get('https://api.imgur.com/3/image/' + imageId + '.json', {
+                        headers: {'Authorization': 'Client-ID ' + ids.imgurID}
+                        }).success(function (response) {
+                            post.url = response.data.link;
+                            console.log(post.url);
+                        });
+                }
+                    
+            }
             return data;
         };
     return {
         getHot: function (subreddit) {
-            console.log('g');
             return $http.get('http://www.reddit.com/r/' + subreddit + '/hot.json').
                 success(function (response) {
-                    var thesePosts = response.data.children;
+                    var thesePosts = processImages(response.data.children);
                     angular.copy(thesePosts, current.hot);
                 });
         },
