9b4eba59d14038a0c70e8c0390bca6d7ec6f0091
Mika Aguilar <mika.aguilar@gmail.com>
2014-11-16 18:37:41 -0600

    Added select box for sort options
    Didn't hook it up yet.

--- public/app/controllers/menuControls.js
+++ public/app/controllers/menuControls.js
@@ -3,6 +3,16 @@
 squareddit.controller('menuControls', ['$scope', 'posts',
     function ($scope, posts) {
         $scope.posts = posts;
+        
+        $scope.sortMethods = [
+            'hot',
+            'new',
+            'rising',
+            'controversial',
+            'top',
+            'gilded'
+        ];
+        
         $scope.updatePosts = function () {
             posts.getPosts(posts.current.sub, 'hot');
         };
--- public/js/squareddit.app.js
+++ public/js/squareddit.app.js
@@ -137,6 +137,16 @@ squareddit.controller('listPosts', ['$scope', '$document', 'posts',
 squareddit.controller('menuControls', ['$scope', 'posts',
     function ($scope, posts) {
         $scope.posts = posts;
+        
+        $scope.sortMethods = [
+            'hot',
+            'new',
+            'rising',
+            'controversial',
+            'top',
+            'gilded'
+        ];
+        
         $scope.updatePosts = function () {
             posts.getPosts(posts.current.sub, 'hot');
         };
