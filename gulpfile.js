2df07071530a07504badbf7e97d6dd6d8bc036e3
aguilarm <mika@favish.com>
2015-06-30 17:00:39 -0500

    chore(*): update with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,84 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var livereload = require('gulp-livereload');
 var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
 
-gulp.task('sass', function () {
-  gulp.src('./public/sass/*.scss')
+var path = {
+  HTML: 'src/index.html',
+  MINIFIED_OUT: 'build.min.js',
+  OUT: 'build.js',
+  DEST: 'dist',
+  DEST_BUILD: 'dist/build',
+  DEST_SRC: 'dist/src',
+  ENTRY_POINT: 'src/jsx/App.jsx',
+  SCSS: 'src/scss',
+  CSS: 'dist/css'
+};
+
+gulp.task('scss', function() {
+  gulp.src(path.SCSS + '/**/*.scss')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public/css')
+    .pipe(gulp.dest(path.CSS)
   );
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('./public/app/**/*.js')
-    .pipe(concat('squareddit.app.js'))
-    .pipe(gulp.dest('./public/js/'));
-});
-
-// Watch Files For Changes
 gulp.task('watch', function() {
-  livereload.listen({host: null});
-  gulp.watch('./public/sass/**/*.scss', ['sass']);
-  gulp.watch('./public/css/**/*.css').on('change', livereload.changed);
-  gulp.watch('./public/app/**/*.js', ['scripts']);
+
+  gulp.watch(path.SCSS, ['scss']);
+
+  gulp.watch(path.DEST).on('change', livereload.changed);
+
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(plumber())
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC));
+    console.log('JSX Built.');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
 });
 
-//Default task
-gulp.task('default', ['scripts', 'sass', 'watch']);
+gulp.task('build', function(){
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify]
+  })
+    .bundle()
+    .pipe(plumber())
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(streamify(uglify(path.MINIFIED_OUT)))
+    .pipe(gulp.dest(path.DEST_BUILD));
+});
+
+gulp.task('replaceHTML', function(){
+  gulp.src(path.HTML)
+    .pipe(htmlreplace({
+      'js': 'build/' + path.MINIFIED_OUT
+    }))
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('production', ['replaceHTML', 'build']);
+
+gulp.task('default', ['watch']);
